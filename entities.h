cf30351fc6647d6a350ae6f91cde0e8c9138331a
Nuno Penim <nuno.penim@gmail.com>
2020-01-27 12:04:44 +0000

    general: reverted

--- entities.h
+++ entities.h
@@ -16,37 +16,3 @@ typedef struct _pack {
     char peso[5];
     char airport[4];
 } package;
-
-int getAirNumber(char * airCode) {
-	if (!strcmp(airCode, "AMS")) {
-		return 0;
-	}
-	else if (!strcmp(airCode, "ATL")) {
-		return 1;
-	}
-	else if (!strcmp(airCode, "DXB")) {
-		return 2;
-	}
-	else if (!strcmp(airCode, "GRU")) {
-		return 3;
-	}
-	else if (!strcmp(airCode, "HKG")) {
-		return 4;
-	}
-	else if (!strcmp(airCode, "ICN")) {
-		return 5;
-	}
-	else if (!strcmp(airCode, "JNB")) {
-		return 6;
-	}
-	else if (!strcmp(airCode, "LGW")) {
-		return 7;
-	}
-	else if (!strcmp(airCode, "MOW")) {
-		return 8;
-	}
-	else if (!strcmp(airCode, "TLS")) {
-		return 9;
-	}
-	return -1;
-}
