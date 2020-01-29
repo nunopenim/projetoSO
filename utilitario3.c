0e5df07e9a091e49f24f08bc4bd42e187135cf91
Nuno Penim <nuno.penim@gmail.com>
2020-01-29 11:25:14 +0000

    utilitario3: finished

--- utilitario3.c
+++ utilitario3.c
@@ -25,29 +25,24 @@ void *logger(void *_args) {
 	int fifoNum = (int) _args;
 	char fifoName[9];
 	char buffer[BUFFERSIZE];
-	sprintf(fifoName, "fifoin%d", fifoNum);
-	FILE* fifo = fopen(fifoName, "r");
-	char c = fgetc(fifo);
-	while(c != EOF) {
-		int i = 0;
-		bufferWipe(buffer, BUFFERSIZE);
-		while(c != '\0' || c != '\n') {
-			buffer[i] = c;
-		}
+	sprintf(fifoName, "fifoout%d", fifoNum);
+	int fifofd = open(fifoName, O_RDONLY);
+	int n;
+	while ((n = read(fifofd, buffer, BUFFERSIZE)) > 0){
 		sem_wait(&outputSem);
-		printf("%s", buffer);
-		write(logfd, buffer, strlen(buffer));
+		write(1, buffer, n);
+		write(logfd, buffer, n);
 		sem_post(&outputSem);
 	}
 }
 
 
 int main() {
+	sem_init(&outputSem, 0, 1);
 	if((logfd = open(logName, O_WRONLY|O_CREAT|O_TRUNC, 0664)) == -1){
 		printf("Error opening/creating file %s\n", logName);
 		return 1;
 	}
-	sem_init(&outputSem, 0, 1);
 	for(int i = 0; i < THREADCOUNT; i++) {
 		pthread_create(&threads[i], NULL, logger, (void *) i);
 	}
